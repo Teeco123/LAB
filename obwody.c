8d90ba62fec95fba929e75bb6dc7f79907dc9418
Kacper Gajko <kacper.gajko1@icloud.com>
2024-11-07 09:47:22 +0100

    Update obwody.c

--- obwody.c
+++ obwody.c
@@ -19,34 +19,53 @@ int main()
     printf("Podaj d:");
     scanf("%d", &d);
 
-    int obwod_kwadratu(){
+    int h;
+    printf("Podaj h:");
+    scanf("%d", &h);
+
+    int kwadrat(){
         int obwod;
+        int pole;
         obwod = a + a + a + a;
+        pole = a * a;
         printf("obwod kwadratu to: %d \n", obwod);
+        printf("pole kwadratu to: %d \n", pole);
+        printf(" \n");
     }
 
-    int obwod_prostokatu(){
+    int prostokat(){
         int obwod;
+        int pole;
         obwod = a + a + b + b;
+        pole = a * b;
         printf("obwod prostakatu to: %d \n", obwod);
+        printf("pole prostakatu to: %d \n", pole);
+        printf(" \n");
     }
 
-    int obwod_trapezu(){
+    int trapez(){
         int obwod;
+        int pole;
         obwod = a + b + c + d;
+        pole = (a + b) * h / 2;
         printf("obwod trapezu to: %d \n", obwod);
+        printf("pole trapezu to: %d \n", pole);
+        printf(" \n");
     }
 
-    int obwod_trojkata(){
+    int trojkat(){
         int obwod;
+        int pole;
         obwod = a + b +c;
+        pole = (a * h) / 2;
         printf("obwod trojkata to: %d \n", obwod);
+        printf("pole trojkata to: %d \n", pole);
     }
 
-    obwod_kwadratu();
-    obwod_prostokatu();
-    obwod_trapezu();
-    obwod_trojkata();
+    kwadrat();
+    prostokat();
+    trapez();
+    trojkat();
 
     return 0;
 }
