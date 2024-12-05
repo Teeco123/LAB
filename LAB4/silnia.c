764da4b3e74c970cddb84ee71890395cbae631d0
Kacper Gajko <kacper.gajko1@icloud.com>
2024-12-05 09:44:03 +0100

    silnia

---
+++ LAB4/silnia.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+  int silnia;
+  int n;
+
+  printf("Podaj liczbe do polczenia silni:");
+  scanf("%d", &n);
+
+  while(n<0){
+    printf("podaj n wieksze od 0\n");
+    scanf("%d", &n);
+  };
+
+  for(int i = 1; i <=n; i++){
+    silnia*=i;
+  }
+
+  printf("silnia liczby %d jest rowna %d\n", n, silnia);
+
+  return 0;
+}
